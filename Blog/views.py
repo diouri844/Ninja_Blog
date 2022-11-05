da00e97a203e39e26683703c71497edbf93c25fa
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-11-05 21:06:42 +0100

    update views router handler

--- Blog/views.py
+++ Blog/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def Blog_home(request):
-	return HttpResponse('<h1> Blog List b</h1>')
+	# rendder the home template : 2 arguments 
+	# arg 1: request object 
+	# arg 2: template path as string  
+	return render(request,'Blog/Home.html')
+
+def Blog_about(request):
+	return render(request,'Blog/About.html')
