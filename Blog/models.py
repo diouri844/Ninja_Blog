131b5f210271fc484c2081c29b4952d90b93ca3a
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-11-12 20:26:01 +0100

    add Blog model & migrations

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+# import user model 
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog(models.Model):
+	# define attributes :
+	title = models.CharField(max_length=50)
+	content = models.TextField()
+	posted_at = models.DateTimeField(auto_now_add=True)
+	# on_delete option manage if the user has been deleted :
+	author = models.ForeignKey(User,on_delete=models.CASCADE)
