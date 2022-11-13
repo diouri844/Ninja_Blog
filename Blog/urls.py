c6f29789991e8207f16ab42ead495ee071c016e6
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-11-13 20:43:03 +0100

    add new urls

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.Blog_home,name='blog-home'),
     path('About/',views.Blog_about, name='about-blog'),
+    path('Login/',views.Blog_login,name='login-blog'),
+    path('Register/',views.Blog_register, name='register-blog'),
 ]
